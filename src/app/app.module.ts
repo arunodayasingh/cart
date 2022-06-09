5293eca505a9db9f5ab6fbcf3290186a2192ed0e
arunodayasingh <arunodayasingh006@gmail.com>
2022-06-09 16:27:43 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { TopBarComponent } from './top-bar/top-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ShoppingCartComponent,
+    ProductListComponent,
+    TopBarComponent
   ],
   imports: [
     BrowserModule
